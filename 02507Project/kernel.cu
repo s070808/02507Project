0b86e86c2dd38f64a3d16e4b7b1393aacf03398e
Kim Harder Fog <oz2kbr@gmail.com>
2016-01-06 12:36:06 +0100

    ASCII X & Y

--- 02507Project/kernel.cu
+++ 02507Project/kernel.cu
@@ -17,33 +17,31 @@ const int N = 32;
 const int blocksize = 16;
 
 __global__
-void hello(device_ptr<char> a, device_ptr<int> b)
-{
-	// unsigned int x = blockIdx.x*blockDim.x + threadIdx.x;
-	// unsigned int y = blockIdx.y*blockDim.y + threadIdx.y;
-	a[threadIdx.x] = a[threadIdx.x] + b[threadIdx.x];
+void hello(device_ptr<char> screen, device_ptr<float> triangle) {
+	unsigned int x = blockIdx.x*blockDim.x + threadIdx.x;
+	unsigned int y = blockIdx.y*blockDim.y + threadIdx.y;
+	screen[N*y + x] = 48 + x;
 }
 
-int main()
-{
-	
-	vector<char> a = { 'H', 'e', 'l', 'l', 'o', ' ' };
-	vector<int> b = { 15, 10, 6, 0, -11, 1 };
-
-	for (size_t i = 0; i < a.size(); i++) {
-		std::cout << a[i];
-	}
-
-	device_vector<char> ad = a;
-	device_vector<int> bd = b;
+int main() {
+	device_vector<char> screen(N*N);
+	vector<float> triangle = {
+		-1.0, -1.0,
+		1.0, -1.0,
+		1.0, 1.0
+	};
+	device_vector<float> triangle_device = triangle;
 
 	dim3 dimGrid(N/blocksize, N/blocksize);
 	dim3 dimBlock(blocksize, blocksize);
 	
-	hello<<<dimGrid, dimBlock>>>(ad.data(), bd.data());
+	hello<<<dimGrid, dimBlock>>>(screen.data(), triangle_device.data());
 
-	for (size_t i = 0; i < ad.size(); i++) {
-		std::cout << ad[i];
+	for (size_t y = 0; y < N; y++) {
+		for (size_t x = 0; x < N; x++) {
+			std::cout << screen[y*N + x];
+		}
+		std::cout << std::endl;
 	}
 
 	std::cout << std::endl;
