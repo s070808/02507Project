ff9e5dac5ce0aac794fd642d610bf939ca659b47
Kim Harder Fog <oz2kbr@gmail.com>
2016-01-08 14:00:53 +0100

    Implemented rasterizer as functor rather than function+lambda

--- 02507Project/kernel.cu
+++ 02507Project/kernel.cu
@@ -47,53 +47,58 @@ void hello(thrust::device_ptr<float> screen, thrust::device_ptr<float> triangle)
 
 __device__
 float rasterize_pixel(const unsigned int pixel_x, const unsigned int pixel_y, const thrust::device_ptr<float> triangle) {
-	float screen_x = (float)pixel_x + 0.5f;
-	float screen_y = (float)pixel_y + 0.5f;
 
-	float point[2] = {
-		(screen_x * 2 - N) / (N - 1),
-		-(screen_y * 2 - N) / (N - 1)
-	};
-
-	float A = 1.f / 2.f * (-triangle[3] * triangle[4] + triangle[1] * (-triangle[2] + triangle[4]) + triangle[0] * (triangle[3] - triangle[5]) + triangle[2] * triangle[5]);
-	float sign = A < 0 ? -1 : 1;
-	float s = (triangle[1] * triangle[4] - triangle[0] * triangle[5] + (triangle[5] - triangle[1]) * point[0] + (triangle[0] - triangle[4]) * point[1]) * sign;
-	float t = (triangle[0] * triangle[3] - triangle[1] * triangle[2] + (triangle[1] - triangle[3]) * point[0] + (triangle[2] - triangle[0]) * point[1]) * sign;
-
-	bool in_triangle = s >= 0 && t >= 0 && ((s + t) <= 2 * A * sign);
-
-	if (in_triangle) {
-		return 1.f;
-	}
-	else {
-		return 0.f;
-	}
 }
 
+struct rasterize_functor {
+	const unsigned int N;
+	const thrust::device_ptr<float> triangle;
+
+	rasterize_functor(const unsigned int N, const thrust::device_ptr<float> triangle) : N(N), triangle(triangle) {}
+
+	__device__
+		float operator()(const unsigned int& i) const {
+			unsigned int pixel_x = i % N;
+			unsigned int pixel_y = i / N;
+			float screen_x = (float)pixel_x + 0.5f;
+			float screen_y = (float)pixel_y + 0.5f;
+
+			float point[2] = {
+				(screen_x * 2 - N) / (N - 1),
+				-(screen_y * 2 - N) / (N - 1)
+			};
+
+			float A = 1.f / 2.f * (-triangle[3] * triangle[4] + triangle[1] * (-triangle[2] + triangle[4]) + triangle[0] * (triangle[3] - triangle[5]) + triangle[2] * triangle[5]);
+			float sign = A < 0 ? -1 : 1;
+			float s = (triangle[1] * triangle[4] - triangle[0] * triangle[5] + (triangle[5] - triangle[1]) * point[0] + (triangle[0] - triangle[4]) * point[1]) * sign;
+			float t = (triangle[0] * triangle[3] - triangle[1] * triangle[2] + (triangle[1] - triangle[3]) * point[0] + (triangle[2] - triangle[0]) * point[1]) * sign;
+
+			bool in_triangle = s >= 0 && t >= 0 && ((s + t) <= 2 * A * sign);
+
+			if (in_triangle) {
+				return 1.f;
+			}
+			else {
+				return 0.f;
+			}
+		}
+};
+
 int main() {
-	thrust::host_vector<float> screen(N*N);
 	thrust::device_vector<float> screen_d(N*N);
 	std::vector<float> triangle = {
 		-0.6f, 1.0f,
 		-1.0f, -0.8f,
 		1.0f, -0.2f
 	};
-	thrust::host_vector<unsigned int> indices(N*N);
 	thrust::device_vector<float> triangle_d = triangle;
 	thrust::device_ptr<float> triangle_data = triangle_d.data();
 
+	thrust::device_vector<unsigned int> indices(N*N);
 	thrust::sequence(indices.begin(), indices.end());
-	thrust::device_vector<unsigned int> indices_d = indices;
-	std::cout << screen_d.size() << " " << indices_d.size() << std::endl;
-	thrust::transform(indices_d.begin(), indices_d.end(), screen_d.begin(), [=] __device__(unsigned int i) {
-		return rasterize_pixel(i%N, i/N, triangle_data);
-	});
+	thrust::transform(indices.begin(), indices.end(), screen_d.begin(), rasterize_functor(N, triangle_data));
 
-	std::cout << indices_d[12] << " " << indices_d[44] << std::endl;
-	std::cout << screen_d[12] << " " << screen_d[44] << std::endl;
-
-	screen = screen_d;
-
+	thrust::host_vector<float> screen = screen_d;
 
 	std::cout << "+-";
 	for (size_t i = 0; i < N; i++)
@@ -105,7 +110,7 @@ int main() {
 	for (size_t y = 0; y < N; y++) {
 		std::cout << "| ";
 		for (size_t x = 0; x < N; x++) {
-			float c = screen[y*N + x];// > 0 ? '#' : '.';
+			char c = screen[y*N + x] > 0 ? '#' : '.';
 			std::cout << c << " ";
 		}
 		std::cout << "|";
