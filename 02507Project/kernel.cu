9007b824e0bdfbb83704dafd9eabe1551d893224
Kim Harder Fog <oz2kbr@gmail.com>
2016-01-13 13:05:59 +0100

    Triangle texture!!

--- 02507Project/kernel.cu
+++ 02507Project/kernel.cu
@@ -3,12 +3,14 @@
 #include <cuda.h>
 #include <vector>
 #include <iostream>
+#include <ctime>
 
 #define GLM_FORCE_CUDA
 #include <glm/glm.hpp>
 #define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include "Shader.h"
 
 #include <thrust/transform.h>
 #include <thrust/functional.h>
@@ -16,24 +18,27 @@
 #include <thrust/device_vector.h>
 #include <thrust/sequence.h>
 
-__device__ const int N = 128;
+__device__ const int N = 1024;
+const unsigned int WIDTH = N, HEIGHT = N;
 
 struct rasterize_functor {
-	const unsigned int N;
+	const unsigned int width;
+	const unsigned int height;
 	const thrust::device_ptr<float> triangle;
 
-	rasterize_functor(const unsigned int N, const thrust::device_ptr<float> triangle) : N(N), triangle(triangle) {}
+	rasterize_functor(const unsigned int width, const unsigned int height, const thrust::device_ptr<float> triangle)
+		: width(width), height(height), triangle(triangle) {}
 
 	__device__
 		float operator()(const unsigned int& i) const {
-			unsigned int pixel_x = i % N;
-			unsigned int pixel_y = i / N;
+			unsigned int pixel_x = i % width;
+			unsigned int pixel_y = i / height;
 			float screen_x = (float)pixel_x + 0.5f;
 			float screen_y = (float)pixel_y + 0.5f;
 
 			float point[2] = {
-				(screen_x * 2 - N) / (N - 1),
-				-(screen_y * 2 - N) / (N - 1)
+				(screen_x * 2 - width) / (width - 1),
+				-(screen_y * 2 - height) / (height - 1)
 			};
 
 			float A = 1.f / 2.f * (-triangle[3] * triangle[4] + triangle[1] * (-triangle[2] + triangle[4]) + triangle[0] * (triangle[3] - triangle[5]) + triangle[2] * triangle[5]);
@@ -52,105 +57,8 @@ struct rasterize_functor {
 		}
 };
 
-void display_pixels(thrust::host_vector<float> screen) {
-	std::cout << "+-";
-	for (size_t i = 0; i < N; i++)
-	{
-		std::cout << "--";
-	}
-	std::cout << "+";
-	std::cout << std::endl;
-	for (size_t y = 0; y < N; y++) {
-		std::cout << "| ";
-		for (size_t x = 0; x < N; x++) {
-			char c = screen[y*N + x] > 0 ? '#' : '.';
-			std::cout << c << " ";
-		}
-		std::cout << "|";
-		std::cout << std::endl;
-	}
-	std::cout << "+-";
-	for (size_t i = 0; i < N; i++)
-	{
-		std::cout << "--";
-	}
-	std::cout << "+";
-	std::cout << std::endl;
-}
-
-GLubyte screenData[N][N][3];
-GLuint gl_texturePtr = 0;
-uchar4* h_textureBufferData = nullptr;
-uchar4* d_textureBufferData = nullptr;
-
-void initBuffers(int width, int height) {
-	// Only needed if we were to initialize buffers multiple times, which we don't do currently.. ¯\_(ツ)_/¯
-	delete[] h_textureBufferData;
-	h_textureBufferData = nullptr;
-	glDeleteTextures(1, &gl_texturePtr);
-	gl_texturePtr = 0;
-
-	h_textureBufferData = new uchar4[width * height];
-
-	glEnable(GL_TEXTURE_2D);
-	glGenTextures(1, &gl_texturePtr);
-	glBindTexture(GL_TEXTURE_2D, gl_texturePtr);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, h_textureBufferData);
-
-	glBindTexture(GL_TEXTURE_2D, 0);
-}
-
-void setupTexture(int width, int height) {
-	// Clear screen
-	for (int y = 0; y < height; ++y) {
-		for (int x = 0; x < width; ++x) {
-			screenData[y][x][0] = screenData[y][x][1] = screenData[y][x][2] = 0;
-		}
-	}
-
-	// Create a texture 
-	glTexImage2D(GL_TEXTURE_2D, 0, 3, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, (GLvoid*)screenData);
-
-	// Set up the texture
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-
-	// Enable textures
-	glEnable(GL_TEXTURE_2D);
-}
-
-void updateTexture(thrust::host_vector<float>& screen) {
-	for (size_t y = 0; y < N; y++) {
-		for (size_t x = 0; x < N; x++) {
-			float pixelf = screen[y*N + x] * 125;
-			screenData[y][x][0] = (GLubyte)230;
-			screenData[y][x][1] = (GLubyte)230;
-			screenData[y][x][2] = (GLubyte)230;
-			std::cout << screenData[y][x][1] << "-";
-		}
-	}
-}
-
-void drawTexture(int width, int height) {
-	glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, (GLvoid*)screenData);
-
-	glBegin(GL_QUADS);
-	glTexCoord2d(0.0, 0.0);		glVertex2d(0.0, 0.0);
-	glTexCoord2d(1.0, 0.0); 	glVertex2d(width, 0.0);
-	glTexCoord2d(1.0, 1.0); 	glVertex2d(width, height);
-	glTexCoord2d(0.0, 1.0); 	glVertex2d(0.0, height);
-	glEnd();
-}
-
-int main() {
-
-	thrust::device_vector<float> screen_d(N*N);
+void generate_image(unsigned char* image) {
+	thrust::device_vector<float> screen_d(WIDTH*HEIGHT);
 
 	// Initialize a triangle from three points
 	std::vector<float> triangle = {
@@ -162,70 +70,165 @@ int main() {
 	thrust::device_vector<float> triangle_d = triangle;
 	thrust::device_ptr<float> triangle_data = triangle_d.data();
 
-	thrust::device_vector<unsigned int> indices(N*N);
+	thrust::device_vector<unsigned int> indices(WIDTH*HEIGHT);
 	thrust::sequence(indices.begin(), indices.end());
-	thrust::transform(indices.begin(), indices.end(), screen_d.begin(), rasterize_functor(N, triangle_data));
+	thrust::transform(indices.begin(), indices.end(), screen_d.begin(), rasterize_functor(WIDTH, HEIGHT, triangle_data));
 
 	thrust::host_vector<float> screen = screen_d;
+	
+	for (int i = 0; i < screen.size(); i++) {
+		float value = screen[i];
+		image[i*3] = value > 0.0f ? 255 : 0;
+		image[i*3 + 1] = value > 0.0f ? 255 : 0;
+		image[i*3 + 2] = value > 0.0f ? 255 : 0;
+	}
+}
 
-	// display_pixels(screen);
+// Function prototypes
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
-	// Initialize GLFW
+// Window dimensions
+
+int main() {
+	// Init GLFW
 	glfwInit();
+	// Set all the required options for GLFW
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	// glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-	// Create a OpenGL window
-	GLFWwindow* window = glfwCreateWindow(N, N, "LearnOpenGL", nullptr, nullptr);
-	if (window == nullptr)
-	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
+	// Create a GLFWwindow object that we can use for GLFW's functions
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
 	glfwMakeContextCurrent(window);
 
-	// Initialize OpenGL functions with GLEW
+	// Set the required callback functions
+	glfwSetKeyCallback(window, key_callback);
+
+	// Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
 	glewExperimental = GL_TRUE;
-	if (glewInit() != GLEW_OK)
-	{
-		std::cout << "Failed to initialize GLEW" << std::endl;
-		return -1;
-	}
+	// Initialize GLEW to setup the OpenGL Function pointers
+	glewInit();
 
-	// Set OpenGL viewport
-	glViewport(0, 0, N, N);
-	glClearColor(0.2, 0.2, 0.2, 1.0);
+	// Define the viewport dimensions
+	glViewport(0, 0, WIDTH, HEIGHT);
 
-	float* pixels = new float[N * N * 3];
 
-	for (size_t y = 0; y < N; y++) {
-		for (size_t x = 0; x < N; x++) {
-			float pixelf = screen[y*N + x]*255;
-			pixels[y*N + x + 0] = pixelf;
-			pixels[y*N + x + 1] = pixelf;
-			pixels[y*N + x + 2] = pixelf;
-			std::cout << pixels[y*N + x + 2] << "-";
-		}
-	}
+	// Build and compile our shader program
+	Shader ourShader("textures.vert", "textures.frag");
 
-	//setupTexture(N, N);
 
-	// Start render loop
+	// Set up vertex data (and buffer(s)) and attribute pointers
+	GLfloat vertices[] = {
+		// Positions          // Colors           // Texture Coords
+		1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // Top Right
+		1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // Bottom Right
+		-1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // Bottom Left
+		-1.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f  // Top Left 
+	};
+	GLuint indices[] = {  // Note that we start from 0!
+		0, 1, 3, // First Triangle
+		1, 2, 3  // Second Triangle
+	};
+	GLuint VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// Position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	// Color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+	// TexCoord attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(2);
+
+	glBindVertexArray(0); // Unbind VAO
+
+
+	// Load and create a texture 
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture); // All upcoming GL_TEXTURE_2D operations now have effect on this texture object
+	// Set the texture wrapping parameters
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// Set texture wrapping to GL_REPEAT (usually basic wrapping method)
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// Set texture filtering parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	// Load image, create texture
+	int widths = WIDTH;
+	int heights = HEIGHT;
+	unsigned char* image = new unsigned char[widths*heights * 3];
+	clock_t begin, end;
+	double elapsed_secs;
+
+	begin = std::clock();
+	generate_image(image);
+	end = std::clock();
+	elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+	std::cout << "First run: " << elapsed_secs*1000.0 << "ms" << std::endl;
+
+	begin = std::clock();
+	generate_image(image);
+	end = std::clock();
+	elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+	std::cout << "Second run: " << elapsed_secs*1000.0 << "ms" << std::endl;
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, widths, heights, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	//glGenerateMipmap(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, 0); // Unbind texture when done, so we won't accidentily mess up our texture.
+
+
+	// Game loop
 	while (!glfwWindowShouldClose(window))
 	{
+		// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
 		glfwPollEvents();
 
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glDrawPixels(N, N, GL_RGB, GL_FLOAT, pixels);
-		//glRasterPos2i(0, 0);
-		//glDrawPixels(N, N, GL_RGB, GL_UNSIGNED_BYTE, screenData);
-		// glFlush();
+		// Render
+		// Clear the colorbuffer
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
 
+
+		// Bind Texture
+		glBindTexture(GL_TEXTURE_2D, texture);
+
+		// Activate shader
+		ourShader.Use();
+
+		// Draw container
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+
+		// Swap the screen buffers
 		glfwSwapBuffers(window);
 	}
-
+	// Properly de-allocate all resources once they've outlived their purpose
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+	// Terminate GLFW, clearing any resources allocated by GLFW.
+	glfwTerminate();
 	return 0;
+}
+
+// Is called whenever a key is pressed/released via GLFW
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
 }
