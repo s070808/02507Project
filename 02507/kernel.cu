60875e4c7fcb30404d9c4ca72f4c2bf117fce035
Kim Harder Fog <oz2kbr@gmail.com>
2016-01-22 15:50:08 +0100

    Last code push after presentation

--- 02507/kernel.cu
+++ 02507/kernel.cu
@@ -3,12 +3,14 @@
 #include "render.h"
 
 int main() {
-	const int width = 512, height = 512;
-	auto use_procedurally_generated_scene = false;
+	const int width = 1024, height = 1024;
+	const int numVertices_x = 72, numVertices_y = 72;
+	auto use_procedurally_generated_scene = true;
+
 	auto image = new unsigned char[width*height * 3];
 	auto scene = use_procedurally_generated_scene
-		? kp::generate_cosine_scene(11, 11)
-		: kp::load_scene("scenes/cube.obj", 1.0f);
+		? kp::generate_cosine_scene(numVertices_x, numVertices_y)
+		: kp::load_scene("scenes/teapot.obj", 1.0f);
 	kp::render_image(image, width, height, scene, 0.25f);
 	return kp::display_image(image, width, height);
 }
